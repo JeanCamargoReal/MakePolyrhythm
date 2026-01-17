92b8bd9025de4e84daa3ac8bce48661f57b66bff
JeanCamargoReal <jeancamargoreal@icloud.com>
2026-01-17 19:05:52 -0300

    Initial Commit

---
+++ MakePolyrhythm/MakePolyrhythmApp.swift
@@ -0,0 +1,17 @@
+//
+//  MakePolyrhythmApp.swift
+//  MakePolyrhythm
+//
+//  Created by Jean Camargo on 17/01/26.
+//
+
+import SwiftUI
+
+@main
+struct MakePolyrhythmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
